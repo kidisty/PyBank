d953100799a4b047b213b796f51ea7ab774cc593
kidist  yohannes <kidistyohannes@kidists-MacBook-Pro.local>
2020-07-07 01:26:24 -0700

    updated files

--- PyBank.py
+++ PyBank.py
@@ -4,7 +4,7 @@
 import csv
 import os
 
-# Files to load and output (Remember to change these)
+# Files to load and output 
 file_to_load = os.path.join("Resources", "budget_data.csv")
 file_to_output = os.path.join("analysis", "budget_analysis.txt")
 
